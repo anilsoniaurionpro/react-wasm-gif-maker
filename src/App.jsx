16785c9d02b604c6485ad6a66b64013b8b258c0a
Anil Soni <anil.sonix@gmail.com>
2021-09-22 11:54:17 +0530

    Adding simple test

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
-const ffmpeg = createFFmpeg({ log: true , corePath: 'static/js/ffmpeg-core.js' });
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import imageData from './images.json';
+
+const ffmpeg = createFFmpeg({
+  log: true,
+  corePath: 'static/js/ffmpeg-core.js',
+});
+console.log(imageData.images.length);
 
 function App() {
   const [ready, setReady] = useState(false);
@@ -12,37 +17,72 @@ function App() {
   const load = async () => {
     await ffmpeg.load();
     setReady(true);
-  }
+  };
 
   useEffect(() => {
     load();
-  }, [])
+  }, []);
 
   const convertToGif = async () => {
-    // Write the file to memory 
+    // Write the file to memory
     ffmpeg.FS('writeFile', 'test.mp4', await fetchFile(video));
 
     // Run the FFMpeg command
-    await ffmpeg.run('-i', 'test.mp4', '-t', '2.5', '-ss', '2.0', '-f', 'gif', 'out.gif');
+    await ffmpeg.run(
+      '-i',
+      'test.mp4',
+      '-t',
+      '2.5',
+      '-ss',
+      '2.0',
+      '-f',
+      'gif',
+      'out.gif',
+    );
 
     // Read the result
     const data = ffmpeg.FS('readFile', 'out.gif');
 
     // Create a URL
-    const url = URL.createObjectURL(new Blob([data.buffer], { type: 'image/gif' }));
-    setGif(url)
+    const url = URL.createObjectURL(
+      new Blob([data.buffer], { type: 'image/gif' }),
+    );
+    setGif(url);
+  };
+
+  function startEncoding() {
+    console.log('start encoding');
+
+       // Write the file to memory
+       ffmpeg.FS('writeFile', 'a.png', await fetchFile(imageData.images[10]));
+       ffmpeg.FS('writeFile', 'b.png', await fetchFile(imageData.images[20]));
+
+
+       // Run the FFMpeg command
+       await ffmpeg.run(
+         '-i',
+         'a.png',
+         '-i',
+         'b.png',
+        " -filter_complex", "hstack" ,"out.jpg"
+       );
+   
+       // Read the result
+       const data = ffmpeg.FS('readFile', 'out.png');
+   
+       // Create a URL
+       const url = URL.createObjectURL(
+         new Blob([data.buffer], { type: 'image/png' }),
+       );
+       console.log({url});
+       //setGif(url);
   }
 
   return ready ? (
-    
     <div className="App">
-      { video && <video
-        controls
-        width="250"
-        src={URL.createObjectURL(video)}>
-
-      </video>}
-
+      {video && (
+        <video controls width="250" src={URL.createObjectURL(video)}></video>
+      )}
 
       <input type="file" onChange={(e) => setVideo(e.target.files?.item(0))} />
 
@@ -50,14 +90,13 @@ function App() {
 
       <button onClick={convertToGif}>Convert</button>
 
-      { gif && <img src={gif} width="250" />}
+      {gif && <img src={gif} width="250" />}
 
+      <button onClick={startEncoding}>start encoding</button>
     </div>
-  )
-    :
-    (
-      <p>Loading...</p>
-    );
+  ) : (
+    <p>Loading...</p>
+  );
 }
 
 export default App;
