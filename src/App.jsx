7354e35cb2aa33f406c7b83f5312658bcdf09de5
anilsoniaurionpro <86872361+anilsoniaurionpro@users.noreply.github.com>
2021-10-20 17:05:02 +0530

    fixx

--- src/App.jsx
+++ src/App.jsx
@@ -120,6 +120,7 @@ function App() {
           clearCanvas: true,
           progressiveLoad: false, // Boolean, only svg renderer, loads dom elements when needed. Might speed up initialization for large number of elements.
           hideOnTransparent: true, //Boolean, only svg renderer, hides elements when opacity reaches 0 (defaults to true)
+          preserveAspectRatio: 'xMidYMid meet'
         },
       });
       // return;
